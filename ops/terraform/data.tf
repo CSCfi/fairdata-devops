08d04f1f4042cda3c16f807f8a1845db84f1c55b
Toni Nurmi <toni.nurmi@csc.fi>
2020-09-03 16:44:03 +0300

    delete terraform and ops folder

--- ops/terraform/data.tf
+++
@@ -1,7 +0,0 @@
-data "openstack_compute_flavor_v2" "compute_flavor_tiny" {
-  name = "standard.tiny"
-}
-data "openstack_images_image_v2" "centos_7" {
-    name = "CentOS-7"
-    visibility = "public"
-}
